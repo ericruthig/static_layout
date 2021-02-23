de0e493bf710f95774e8de2325ebd8768dc1d515
unknown <iam@ericruthig.com>
2021-02-23 16:43:57 -0600

    first commit

---
+++ js.js
@@ -0,0 +1,16 @@
+var modal = document.getElementById("imgModal");
+
+var img = document.getElementById("img1");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+} 
