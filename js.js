d136c007ea0715fda46c736d20388cc670cf0e85
ericruthig <74027944+ericruthig@users.noreply.github.com>
2021-02-23 21:31:15 -0600

    Add files via upload

--- js.js
+++ js.js
@@ -1,3 +1,24 @@
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+ 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 var modal = document.getElementById("imgModal");
 
 var img = document.getElementById("img1");
@@ -24,3 +45,7 @@ window.onclick = function(event) {
       modal.style.display = "none";
     }
 };
+function onClick(element) {
+    document.getElementById("details").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+  }
